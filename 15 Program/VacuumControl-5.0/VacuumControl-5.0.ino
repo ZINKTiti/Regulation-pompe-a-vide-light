c6ddee2bc9af21ab18a10eb6ab39a06b52eabfc1
ZINKTiti <zinktiti@hotmail.fr>
2024-03-24 23:10:23 +0100

    Modification PIN Bouton
    Modification PIN bouton pour les interuptes

---
+++ 15 Program/VacuumControl-5.0/VacuumControl-5.0.ino
@@ -0,0 +1,243 @@
+// Vacuum controller, origin Tryggve Kallhovd 2016.
+// Vacuum Controller, modification ZINK Thierry 2024.
+
+
+#include <EEPROM.h>
+#include <Encoder.h>              // Encoder Library: https://www.pjrc.com/teensy/td_libs_Encoder.html
+#include <LiquidCrystal_I2C.h>
+
+// LCD definition
+
+#define I2C_ADDR 0x27                   //Lcd i2c addresse 0x27
+LiquidCrystal_I2C lcd(I2C_ADDR,16,2);   //Instance Afficheur LCD, i2c addresse 0x27, 16 caracères sur 2 lignes
+
+/* Définition non utilisées dans le code
+#define BACKLIGHT_PIN 3
+#define En_pin 2
+#define Rw_pin 1
+#define Rs_pin 0
+#define D4_pin 4
+#define D5_pin 5
+#define D6_pin 6
+#define D7_pin 7
+*/
+
+// Pin definitions:
+
+Encoder thrEnc(2, 3);   // Instance encodeur port 2 (interrupt) et 3 (interrupt)
+int encButPin = 4;      // Click encodeur central
+int relayPin = 11;      // Pin command du relay
+int vacSensPin = A0;    // Pin sensor MPX5100_DP
+
+
+// Variables definitions:
+
+int addr1 = 0; //Adresse EEPROM Valeur Règlage 
+int addr2 = 4; //Adresse EEPROM Valeur Marge
+int value1;
+int value2;
+
+boolean encBut = 1;
+boolean oldBut = 1;
+boolean encSel = 1;
+
+int encPos1;
+int oldPos1;
+int encPos2;
+int oldPos2;
+
+boolean vacSwitch;
+
+int vacuum;
+int vacVal;
+int oldVal;
+
+// Fonctions definitions:
+
+void writeIntIntoEEPROM(int address, int number)
+{ 
+  byte byte1 = number >> 8;
+  byte byte2 = number & 0xFF;
+  EEPROM.write(address, byte1);
+  EEPROM.write(address + 1, byte2);
+}
+
+int readIntFromEEPROM(int address)
+{
+  byte byte1 = EEPROM.read(address);
+  byte byte2 = EEPROM.read(address + 1);
+  return (byte1 << 8) + byte2;
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  lcd.init();       // initialize the lcd
+  lcd.backlight();  // Activer le rétroéclairage;
+  lcd.setCursor(0, 1);
+  //lcd.print("Vacuum:");
+  lcd.print("Mesure:");
+  lcd.setCursor(13, 1);
+  lcd.print("mBr");
+
+  //Initialisation par lecture des valeurs dans l'EEPROM
+  
+  value1 = readIntFromEEPROM(addr1);
+  value2 = readIntFromEEPROM(addr2);
+  encPos1 = (value1 * 4);
+  encPos2 = (value2 * 4);
+
+  // Initialisation des port
+  
+  pinMode(encButPin, INPUT);
+  digitalWrite(encButPin, HIGH);
+
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+
+  pinMode(vacSensPin, INPUT);
+  
+}
+void loop() {
+
+  // Analyse le Bouton de l'encodeur:
+  // change entre "Règlage" (la consigne) et "Marge" (l'hystérésis) à chaque appui
+  // encSel = 1 affiche la consigne
+  // encSel = 0 affiche l'hystérésis
+  // Sur l'afficheur en ligne 0 affichage de la consigne et d l'hystérésis alternativement
+  // La valeur affichée peut être ajustée avec l'encodeur
+  // Sur l'afficheur en ligne 1 affichage de la valeur mesurée
+
+  encBut = digitalRead(encButPin);
+  if (encBut != oldBut) {
+    if (encBut == 0) {
+      encSel = !encSel;
+    }
+    // Affiche la consigne
+    if (encSel == 1) {
+      thrEnc.write(encPos1);
+      lcd.setCursor(0, 0);
+      lcd.print("Reglage:   -");
+
+    } else if (encSel == 0) {   //Affiche l'hystérésis
+      thrEnc.write(encPos2);
+      lcd.setCursor(0, 0);
+      lcd.print("Marge:  +/- ");
+      lcd.setCursor(14, 0);
+      lcd.print(" ");
+    }
+    oldBut = encBut;
+  }
+
+  // Lecture de l'encodeur:
+
+  if (encSel) {
+    encPos1 = thrEnc.read();
+    if (encPos1 != oldPos1) {
+      oldPos1 = encPos1;
+    }
+    
+    // Si l'encodeur a été tourné et encSel = 1 (Régage de la consigne)
+    // Test des des valeurs limites de la consigne qui doit être >= 10 et <= 99
+    // Si en dehors des limites la consigne est mis à la limite correspondante
+    if (encPos1 >= (99 * 4)) {
+      encPos1 = (99 * 4);
+      thrEnc.write(99 * 4);
+    } else if (encPos1 <= (10 * 4)) {
+      encPos1 = (10 * 4);
+      thrEnc.write(10 * 4);
+    }
+    
+    // La consigne est mise en mémoire dans l'EEPROM et affichée
+    int val = ((encPos1) / 4);
+    writeIntIntoEEPROM(addr1, val);
+    lcd.setCursor(12, 0);
+    lcd.print((encPos1 / 4) * 10);
+  }
+  else  {
+    encPos2 = thrEnc.read();
+    if (encPos2 != oldPos2) {
+      oldPos2 = encPos2;
+    }
+
+    // Si l'encodeur a été tourné et encSel = 0 (Régage de l'hystérésis)
+    // Test des valeurs limites de l'hystérésis qui doit être >= 5 et <= 50
+    // Si en dehors des limites l'hystérésis est mise à la limite correspondante  
+    if (encPos2 >= (50 * 4)) {
+      encPos2 = (50 * 4);
+      thrEnc.write(50 * 4);
+    } else if (encPos2 <= (5 * 4)) {
+      encPos2 = (5 * 4);
+      thrEnc.write(5 * 4);
+    }
+    
+    // La valeur de l'hystérésis est mise en mémoire dans l'EEPROM et affichée
+    int val = ((encPos2) / 4);
+    writeIntIntoEEPROM(addr2, val);
+    lcd.setCursor(12, 0);
+    lcd.print(encPos2 / 4);
+    if ((encPos2 / 4) < 10) {
+      lcd.setCursor(13, 0);
+      lcd.print(" ");
+    }
+  }
+
+  // Contrôle du relai en fonction de la valeur de vide mesurée, de la consigne "encPos1" et de l'hytérésis "encPos2":
+
+  if (vacSwitch == 1) {
+    if (vacuum >= ((encPos1 / 4) * 10) + (encPos2 / 4)) {
+      vacSwitch = 0;
+    }
+  } else if (vacSwitch == 0) {
+    if (vacuum < ((encPos1 / 4) * 10) - (encPos2 / 4)) {
+      vacSwitch = 1;
+    }
+    digitalWrite(relayPin, vacSwitch);
+  }
+
+  // mesure de la valeur avec le capteur MPX5100DP
+
+  vacuum = 0;
+  
+  // mesure de 10 valeurs du vide
+  for (byte i = 0; i < 10; i++) {
+    vacuum += analogRead(vacSensPin);
+  }
+  vacuum /= 10;
+  
+  // Mise à l'échelle réelle
+  vacuum = map(vacuum, 41, 962, 0, 1000);
+
+  // Moyennage avec la valeur précédent
+  vacVal = (vacuum + oldVal) / 2;
+  oldVal = vacVal;
+  if (vacVal < 1) {
+    vacVal = 0;
+  }
+  
+  // Affichage de la nouvelle valeur
+  lcd.setCursor(9, 1);
+  lcd.print(vacVal);
+  /*
+  Serial.print(vacVal);
+  Serial.print("_");
+  Serial.print(readIntFromEEPROM(addr1));
+  Serial.print("_");
+  Serial.println(readIntFromEEPROM(addr2));
+  */
+  if (vacVal > 0) {
+    lcd.setCursor(8, 1);
+    lcd.print("-");
+  } else {
+    lcd.setCursor(8, 1);
+    lcd.print(" ");
+  }
+  if (vacVal < 10) {
+    lcd.setCursor(10, 1);
+    lcd.print(" ");
+  } else if (vacVal < 100) {
+    lcd.setCursor(11, 1);
+    lcd.print(" ");
+  }
+}
